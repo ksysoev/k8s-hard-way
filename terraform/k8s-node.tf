65c7249ceb6d69cb5206ca22405ddb4f19a923dd
Kirill Sysoev <k.sysoev@me.com>
2026-07-19 13:52:39 +0700

    Adds second node joining the cluster

--- terraform/k8s-node.tf
+++ terraform/k8s-node.tf
@@ -51,3 +51,33 @@ resource "digitalocean_droplet" "k8smaster" {
   }
 }
 
+resource "digitalocean_droplet" "k8snode" {
+  image = "ubuntu-26-04-x64"
+  name = "k8snode"
+  region = "sgp1"
+  size = "s-2vcpu-2gb"
+  ssh_keys = [
+    resource.digitalocean_ssh_key.k8shw.id
+  ]
+  
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+  provisioner "file" {
+    source = "scripts/bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh /tmp/init_cluster.sh",
+      "/tmp/bootstrap.sh",
+      "kubeadm join ${digitalocean_droplet.k8smaster.ipv4_address_private}:6443 --token \"${local.custom_token}\" --discovery-token-unsafe-skip-ca-verification",
+    ]
+  }
+}
